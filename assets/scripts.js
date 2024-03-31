b3d0193371d8f30ada3c521a4c10b42c9c3f37a8
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-03-31 22:09:59 +0700

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -18,13 +18,10 @@ function pickRandomEmoji() {
 function calculateAge(birthDate) {
     const today = new Date();
     const birth = new Date(birthDate);
-
     let age = today.getFullYear() - birth.getFullYear();
     const monthDiff = today.getMonth() - birth.getMonth();
-
     if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
         age--;
     }
-
     return age;
 }
