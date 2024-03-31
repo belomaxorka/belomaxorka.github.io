593f8a4742e01cf6eade5652f76125d6e4ee846f
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-03-31 17:39:14 +0700

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,5 @@
 function pickRandomEmoji() {
     // https://gist.github.com/ikr7/c72843556ef3a12014c3
-    let emojis = ['🟢', '💚', '📗'];
+    let emojis = ['🟢', '🟩', '💚', '📗', '👽', '🍀', '🍏'];
     return emojis[Math.floor(Math.random() * emojis.length)];
 }
