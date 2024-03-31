940b81bce9d0c775a1be0d8672261c0bfcb69b57
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-03-31 21:32:04 +0700

    🕊

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,10 @@
-
+/**
+ * Random emoji picker
+ *
+ * @link https://gist.github.com/ikr7/c72843556ef3a12014c3
+ * @returns {string}
+ */
+function pickRandomEmoji() {
+    let emojis = ['✨', '🪐', '💫', '☄️', '🌙', '🌏', '🌈', '🌚', '🌝', '🌞', '❄️', '💥'];
+    return emojis[Math.floor(Math.random() * emojis.length)];
+}
